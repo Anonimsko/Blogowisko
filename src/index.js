9bf87ebdd55f7e7bcdd90953c46d7cecc119f75c
Łukasz Król <96410823+krolixON@users.noreply.github.com>
2022-04-24 19:54:28 +0200

    Krok 5

--- src/index.js
+++ src/index.js
@@ -61,13 +61,30 @@ Wystarczy duży zapał, pomysł na treść i dzięki łatwości korzystania z fu
     const [login, setLogin] = useState("");
     const [password, setPassword] = useState("");
     const [isCorrect, setIsCorrect] = useState("");
+    function checkLoginData()
+    {
+      let correctLogin = 1234;
+      let correctPassword = 1234;
+      if(login == correctLogin && password == correctPassword)
+      {
+        setLoginButton("Wyloguj");
+        setLogged(true);
+        setAddBlogButton(<button onClick={() => {  }}>Zarządzaj blogami</button>);
+        setIsCorrect("");
+        setAddBlogButton("Zarządaj blogami")
+        setPage(<MainPage />)
+      }
+      else if(login != correctLogin || password != correctPassword)
+        setIsCorrect(<p>Dane logowania są nieprawidłowe!</p>);
+    }
 
     return(
       <div className="login">
         <h1>Zaloguj się</h1>
-        <input type="text" placeholder="Login"/><br />
-        <input type="password" placeholder="Hasło"/><br />
-        <button>Zaloguj</button><br />
+        <input type="text" value={login} onChange={(e) => setLogin(e.target.value)} placeholder="Login"/><br />
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Hasło"/><br />
+        <button onClick={checkLoginData}>Zaloguj</button><br />
+        {isCorrect}
       </div>
     )
   }
@@ -82,26 +99,49 @@ Wystarczy duży zapał, pomysł na treść i dzięki łatwości korzystania z fu
       const [headline, setHeadline] = useState("");
       const [text, setText] = useState("");
       const [isEmpty, setIsEmpty] = useState("");
+      let date = new Date().toLocaleDateString("pl-PL", {year: "numeric", month: "2-digit", day: "2-digit"});
+
+      const createBlog = async () =>
+      {
+        if(name == "" || headline == "" || text == "")
+          setIsEmpty("Żadne pole nie może być puste!");
+        else
+        {
+          setIsEmpty("");
+          await addDoc(blogsCollectionRef, {name: name, headline: headline, text: text, date: date});
+          getBlogs();
+          setPage(<MainPage />);
+        }
+      }
 
       return(
         <div className="addBlog">
-          <input type="text" placeholder="Nazwa"/><br />
-          <input type="text" placeholder="Nagłówek"/><br />
-          <textarea placeholder="Tekst"/><br />
-          <button>Dodaj</button><br />
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Nazwa"/><br />
+          <input type="text" value={headline} onChange={(e) => setHeadline(e.target.value)} placeholder="Nagłówek"/><br />
+          <textarea value={text} onChange={(e) => setText(e.target.value)} placeholder="Tekst"/><br />
+          <button onClick={createBlog}>Dodaj</button><br />
+          <p>{isEmpty}</p>
         </div>
       )
     }
 
     const DeleteBlog = () =>
     {
+      const deleteBlog = async (id) =>
+      {
+        const blogDoc = doc(db, "blogs", id);
+        await deleteDoc(blogDoc);
+        getBlogs();
+        setPage(<MainPage />);
+      }
+
       return(
         <div className="deleteBlogs">
           {blogs.map((blog, idx) => 
           {
             return(
               <div key={idx}>
-                <h3>{blog.name}</h3><button>Usuń</button><br />
+                <h3>{blog.name}</h3><button onClick={() => { deleteBlog(blog.id) }}>Usuń</button><br />
               </div>
             )
           })}
